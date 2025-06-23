3dec8b5b28b1826e5f317b21339f5de538a02e93
Katelynn Lee <katelynnrae16@gmail.com>
2025-06-23 11:05:46 -0400

    Add move to collection feature on wishlist

--- src/components/ShoeList/ShoeList.tsx
+++ src/components/ShoeList/ShoeList.tsx
@@ -1,4 +1,4 @@
-import type { Shoe } from '../../lib/sharedTypes'
+import type { Shoe, FormDataObj } from '../../lib/sharedTypes'
 import { usePersistedState, usePersistedReducer } from '../../lib/hooks'
 import { reducer, recalculateDailyValues, priceToNumber } from '../../lib/util'
 
@@ -13,6 +13,9 @@ import { FaRegTrashAlt } from "react-icons/fa";
 import AddShoe from './AddShoe'
 import EditShoe from './EditShoe'
 
+// TODO: Switch wishlist shoes to collection
+// TODO: sort function for each column
+
 interface Props {
     page: string
 }
@@ -26,6 +29,7 @@ export default function ShoeList (props: Props) {
     const [dailyValues, setDailyValues] = usePersistedState(`DAILY_VALUES/${props.page}`, [])
 
     const dispatch = usePersistedReducer(reducer, initialState, storageKey)
+    const dispatch2 = usePersistedReducer(reducer, initialState, 'SHOE_COLLECTION/collection')
 
     function handleDelete(id: string) {
         const shoeToDelete: Shoe = storedShoes.shoes.filter((shoe: Shoe) => shoe.id === id)[0]
@@ -33,6 +37,24 @@ export default function ShoeList (props: Props) {
 
         dispatch({ type: 'DELETE_SHOE', payload: id })
         window.location.reload();
+    }
+
+    function handleMove(id: string) {
+        if (props.page === 'collection') return
+        
+        const shoe: Shoe = storedShoes.shoes.filter((shoe: Shoe) => shoe.id === id)[0]
+        const newShoe: FormDataObj = {
+            brand: shoe.brand,
+            model: shoe.model,
+            color: shoe.color,
+            buyDate: new Date().toLocaleDateString(),
+            buyPrice: priceToNumber(shoe.buyPrice),
+            sellPrice: priceToNumber(shoe.sellPrice),
+            link: shoe.link
+        }
+        dispatch2({ type: 'ADD_SHOE', payload: newShoe })
+
+        handleDelete(id)
     }
 
     return (
@@ -46,7 +68,7 @@ export default function ShoeList (props: Props) {
                                 <th><small className="text-muted">Brand</small></th>
                                 <th><small className="text-muted">Model</small></th>
                                 <th><small className="text-muted">Color</small></th>
-                                <th><small className="text-muted">Buy Date</small></th>
+                                {props.page === 'collection' && (<th><small className="text-muted">Buy Date</small></th>)}
                                 <th><small className="text-muted">Buy Price</small></th>
                                 <th><small className="text-muted">Sell Price</small></th>
                                 <th><small className="text-muted">Profit</small></th>
@@ -60,13 +82,18 @@ export default function ShoeList (props: Props) {
                                     <td>{value.brand}</td>
                                     <td>{value.model}</td>
                                     <td>{value.color}</td>
-                                    <td>{value.buyDate}</td>
+                                    {props.page === 'collection' && (<td>{value.buyDate}</td>)}
                                     <td>{value.buyPrice}</td>
                                     <td>{value.sellPrice}</td>
                                     <td>{value.profit}</td>
                                     <td><a href={value.link} target="_blank" className="link-success">{value.domain}</a></td>
                                     <td>
                                         <ButtonGroup className="float-end">
+                                            {props.page === 'wishlist' && (
+                                                <Button onClick={() => handleMove(value.id)} variant="outline-secondary"> 
+                                                    Move to Collection 
+                                                </Button>
+                                            )}
                                             <EditShoe shoe={value} page={props.page}/>
                                             <Button onClick={() => handleDelete(value.id)} variant="outline-secondary"><FaRegTrashAlt /></Button>
                                         </ButtonGroup>
