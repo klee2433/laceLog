d7f6f81281341064b3ec62244260f9bddcd4f43c
Katelynn Lee <katelynnrae16@gmail.com>
2025-06-23 19:44:45 -0400

    Include Electron

---
+++ electron.js
@@ -0,0 +1,15 @@
+import { app, BrowserWindow } from 'electron'
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadURL('http://localhost:5173')
+}
+
+app.whenReady().then(createWindow)
